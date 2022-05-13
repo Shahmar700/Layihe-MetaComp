d0f257eedb0afe5434c3a54c7db581eb26acb20d
Shahmar <shahmar700@gmail.com>
2022-05-13 16:18:43 +0400

    init commit

---
+++ src/Companents/ScrollTopBtn.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { useEffect, useState } from "react";
+
+function ScrollTopBtn() {
+   
+
+  return (
+    <div className="ScrollTopBtn">
+      <i class="fa-solid fa-arrow-up"></i>
+    </div>
+  );
+}
+
+export default ScrollTopBtn;
